0f1553a24aa35169ce0fcd342182dbae044f5369
bIpOlAr <117316947+yPyetroXP@users.noreply.github.com>
2024-05-31 15:21:53 -0300

    Updateddd main.lua

--- main.lua
+++ main.lua
@@ -28,3 +28,34 @@ local Window = Rayfield:CreateWindow({
 
 local MainTab = Window:CreateTab("🏠 Aba Inicial", nil) -- Title, Image
 local MainSection = MainTab:CreateSection("Inicio")
+
+local Button = MainTab:CreateButton({
+    Name = "Ativar Pulo Infinito",
+    Callback = function()
+        --Toggles the infinite jump between on or off on every script run
+ _G.infinjump = not _G.infinjump
+ 
+ if _G.infinJumpStarted == nil then
+     --Ensures this only runs once to save resources
+     _G.infinJumpStarted = true
+     
+     --Notifies readiness
+     game.StarterGui:SetCore("SendNotification", {Title="Pika Hub"; Text="Pulo Infinito ativado!"; Duration=5;})
+ 
+     --The actual infinite jump
+     local plr = game:GetService('Players').LocalPlayer
+     local m = plr:GetMouse()
+     m.KeyDown:connect(function(k)
+         if _G.infinjump then
+             if k:byte() == 32 then
+             humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+             humanoid:ChangeState('Jumping')
+             wait()
+             humanoid:ChangeState('Seated')
+             end
+         end
+     end)
+ end
+    end,
+ })
+ 
