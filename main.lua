3218b36929b6739112d34baf8f94cb8aa34d293d
bIpOlAr <117316947+yPyetroXP@users.noreply.github.com>
2024-05-31 20:39:15 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -182,8 +182,8 @@ local Button3 = MainTab:CreateButton({
                     local Billboard = Instance.new("BillboardGui")
                     Billboard.Name = "ESP"
                     Billboard.AlwaysOnTop = true
-                    Billboard.Size = UDim2.new(4, 0, 5, 0)
-                    Billboard.StudsOffset = Vector3.new(0, 3, 0)
+                    Billboard.Size = UDim2.new(0, 100, 0, 20) -- Tamanho do ESP
+                    Billboard.StudsOffset = Vector3.new(0, 2, 0) -- Posição relativa ao centro do personagem
                     Billboard.Adornee = player.Character.Head
 
                     local Frame = Instance.new("Frame")
@@ -226,6 +226,7 @@ local Button3 = MainTab:CreateButton({
         end
     end,
 })
+
 
 
 
