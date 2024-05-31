dafb5d6252c44e5a818235b9970703d9f574cf81
bIpOlAr <117316947+yPyetroXP@users.noreply.github.com>
2024-05-31 15:12:06 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ local Window = Rayfield:CreateWindow({
        Invite = "https://discord.gg/XYVyF2fGae", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
-    KeySystem = false, -- Set this to true to use our key system
+    KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
        Title = "Pika Hub | Key System",
        Subtitle = "Sistema de Key do Script",
@@ -22,7 +22,7 @@ local Window = Rayfield:CreateWindow({
        FileName = "pikahubkey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+       Key = {"release"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
  })
 
