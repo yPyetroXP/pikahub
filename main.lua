e76f9ec670c43c001e8aa1a06253cde8d57a367b
bIpOlAr <117316947+yPyetroXP@users.noreply.github.com>
2024-10-31 20:03:57 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -25,7 +25,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "PikaHubKey1", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/AtgzSPWK"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"https://pastebin.com/pCwSNNar"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
